f2da7c63712c2604a6ee8f0690d54ebb749d4837
taaaf11 <taafuuu@gmail.com>
2024-08-11 07:27:05 +0500

    Basic logic complete

---
+++ src/break_notif/__main__.py
@@ -0,0 +1,4 @@
+from break_notif.main import runner
+
+if __name__ == "__main__":
+    runner()
--- src/break_notif/main.py
+++ src/break_notif/main.py
@@ -4,6 +4,8 @@ import time
 from break_notif.utils import min_to_sec
 
 
+# TODO: Use notification daemons
+
 async def water_notif() -> None:
     while True:
         minutes = 40
@@ -17,3 +19,12 @@ async def eye_20_rule() -> None:
         await asyncio.sleep(min_to_sec(minutes))
         print("20 rule act")
 
+
+async def main() -> None:
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(water_notif())
+        tg.create_task(eye_20_rule())
+
+
+def runner() -> None:
+    asyncio.run(main())
