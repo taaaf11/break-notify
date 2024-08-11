769458f9a826719b4aadf914f7a065fa1a8adf78
taaaf11 <taafuuu@gmail.com>
2024-08-11 07:57:11 +0500

    Switch to notify_py for sending notifs

--- src/break_notif/main.py
+++ src/break_notif/main.py
@@ -1,23 +1,29 @@
 import asyncio
 import time
 
+#  import notify
+from notifypy import Notify
+
 from break_notif.utils import min_to_sec
 
 
-# TODO: Use notification daemons
-
 async def water_notif() -> None:
+    summary = "Drink water."
+    message = "Drink half a glass of water."
     while True:
         minutes = 40
-        await asyncio.sleep(min_to_sec(minutes))
-        print("drink water")
+        await asyncio.sleep(5)
+        Notify(summary, message).send()
 
 
 async def eye_20_rule() -> None:
+    summary = "Eye care break"
+    message = "Focus your eyes something 20 feet away for 20 minutes."
     while True:
         minutes = 20
-        await asyncio.sleep(min_to_sec(minutes))
-        print("20 rule act")
+        #  await asyncio.sleep(min_to_sec(minutes))
+        await asyncio.sleep(8)
+        Notify(summary, message).send()
 
 
 async def main() -> None:
